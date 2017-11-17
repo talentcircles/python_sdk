5d4783a7aa577287000392b78d2c212a68f3d826
Tom Clowers <tclowers@gmail.com>
2017-11-17 12:56:39 -0600

    working on packaging for PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'talentcircles',
+  packages = ['talentcircles'],
+  version = '0.1',
+  description = 'A module for accessing the TalentCircles REST API',
+  author = 'Tom Clowers',
+  author_email = 'tom@talentcircles.com',
+  url = 'https://github.com/talentcircles/python_sdk',
+  download_url = 'https://github.com/talentcircles/python_sdk/archive/0.1.tar.gz',
+  keywords = ['hr', 'talent', 'jobs'],
+  classifiers = [],
+)
