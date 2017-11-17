d6ace9d808aac2dc1c5a5d2826d33dabc88035e5
Tom Clowers <tclowers@gmail.com>
2017-11-17 17:42:56 -0600

    reverting

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'talentcircles',
   packages = ['talentcircles'],
-  version = '0.4',
+  version = '1.0.4',
   description = 'A module for accessing the TalentCircles REST API',
   author = 'Tom Clowers',
   author_email = 'tom@talentcircles.com',
   url = 'https://github.com/talentcircles/python_sdk',
-  download_url = 'https://github.com/talentcircles/python_sdk/archive/0.4.tar.gz',
+  download_url = 'https://github.com/talentcircles/python_sdk/archive/1.0.4.tar.gz',
   keywords = ['hr', 'talent', 'jobs'],
   license='MIT',
   classifiers = [
