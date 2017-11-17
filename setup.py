21ef68721025ad441757c4cecfac10019f33b23e
Tom Clowers <tclowers@gmail.com>
2017-11-17 15:46:04 -0600

    removing json from required packages

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'talentcircles',
   packages = ['talentcircles'],
-  version = '0.3',
+  version = '0.4',
   description = 'A module for accessing the TalentCircles REST API',
   author = 'Tom Clowers',
   author_email = 'tom@talentcircles.com',
   url = 'https://github.com/talentcircles/python_sdk',
-  download_url = 'https://github.com/talentcircles/python_sdk/archive/0.3.tar.gz',
+  download_url = 'https://github.com/talentcircles/python_sdk/archive/0.4.tar.gz',
   keywords = ['hr', 'talent', 'jobs'],
   license='MIT',
   classifiers = [
@@ -19,7 +19,12 @@ setup(
     'License :: OSI Approved :: MIT License',
 
     'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 2.7'
   ],
-  py_modules=["talentcircles"],
+  install_requires=[
+    'requests',
+    'datetime',
+    'urllib3'
+  ],
+  py_modules=["talentcircles"]
 )
