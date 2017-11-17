85b2d991c31c3a70efad23a85109431c8cfac8ed
Tom Clowers <tclowers@gmail.com>
2017-11-17 13:39:04 -0600

    updating requirements

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'talentcircles',
   packages = ['talentcircles'],
-  version = '0.1',
+  version = '0.3',
   description = 'A module for accessing the TalentCircles REST API',
   author = 'Tom Clowers',
   author_email = 'tom@talentcircles.com',
   url = 'https://github.com/talentcircles/python_sdk',
-  download_url = 'https://github.com/talentcircles/python_sdk/archive/0.1.tar.gz',
+  download_url = 'https://github.com/talentcircles/python_sdk/archive/0.3.tar.gz',
   keywords = ['hr', 'talent', 'jobs'],
   license='MIT',
   classifiers = [
@@ -21,13 +21,11 @@ setup(
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
   ],
-  packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
   install_requires=[
     'requests',
     'datetime',
     'urllib3',
     'json',
   ],
-  python_requires='>=2.6, <3',
   py_modules=["talentcircles"],
 )
