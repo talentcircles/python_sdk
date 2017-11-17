c56f417ea35b1424569ff452201abfcf648a0856
Tom Clowers <tclowers@gmail.com>
2017-11-17 13:22:02 -0600

    updating file structure

--- setup.py
+++ setup.py
@@ -9,5 +9,25 @@ setup(
   url = 'https://github.com/talentcircles/python_sdk',
   download_url = 'https://github.com/talentcircles/python_sdk/archive/0.1.tar.gz',
   keywords = ['hr', 'talent', 'jobs'],
-  classifiers = [],
+  license='MIT',
+  classifiers = [
+    'Development Status :: 5 - Production/Stable',
+
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+
+    'License :: OSI Approved :: MIT License',
+
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+  ],
+  packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+  install_requires=[
+    'requests',
+    'datetime',
+    'urllib3',
+    'json',
+  ],
+  python_requires='>=2.6, <3',
+  py_modules=["talentcircles"],
 )
